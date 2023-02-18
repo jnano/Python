9903e19fe85de195a4094552db469393a0dcd9d6
hunskang <hunskang@gmail.com>
2023-02-18 12:28:31 +0900

    initial project version

--- default.py
+++ default.py
@@ -14,3 +14,12 @@ iphon_repository_dir = ""
 # > git add *.py
 # > git add LICENSE
 # > git commit -m 'initial project version'
+
+# 1. 파일의 상태 확인하기
+# > git status
+
+# 2. 파일을 새로 추적하기
+# > git add [file_name]
+
+# 3. Modified 상태의 파일을 Stage하기
+# > git status
