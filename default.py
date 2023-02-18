d0992d66a135150878a169f55e6bfa6b35a48b9e
hunskang-19 <hunskang@hunskang-19ui-MacBookPro.local>
2023-02-18 11:30:51 +0900

    initial project version

---
+++ default.py
@@ -0,0 +1,10 @@
+# Created on iPad.
+
+print ('Hello World!')
+
+
+#git local repository dir
+labtop_repository_dir = "/Users/hunskang/Library/Mobile Documents/com~apple~CloudDocs/Git/Python"
+ipad_repository_dir = ""
+iphon_repository_dir = ""
+
